67ccc5cb1384161feff9b4f89133c6f508c25a08
xDenieru <evandar-elda@hotmail.com>
2014-07-09 05:15:43 +0200

    Added awards system
    -

---
+++ LODJ Awards.js
@@ -0,0 +1,62 @@
+//<![CDATA[
+var t_award = {
+	name : "Awards",
+	thumbnail : [20,20],
+	closeFunction : "fade",
+	users : [
+// Clan list
+	[3704867,
+	"Highly Trusted","http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/highlytrusted01_zpsbb80c94e.png",
+	"Highly trusted coin swapper",
+	"June 03/2014"],
+		
+	[3704867,
+	"Recruitment Leader",
+	"http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/recruitleader_zpsc73bd81d.png",
+	"Head of Recruitment",
+	"June 03/2014"],
+//Redraven242
+	[3604214,
+	"Guide Manager",
+	"http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/guidemanager02_zps06a4bf9d.png",
+	"Head of Guide Management",
+	"June 06/2014"],
+	
+	[3604214,
+	"Heart Of Gold",
+	"http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/heartofgold04_zps872109ae.png",
+	"Always there for us",
+	"June 06/2014"],
+//Adam
+	[3704872,
+	"Group Leader",
+	"http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/groupleader01_zpsd8e88690.png",
+	"Head of Content Team",
+	"June 25/2014"],
+//Clayshot
+	[3857840,
+	"Clan Herbalist",
+	"http://static2.wikia.nocookie.net/__cb20130228042634/2007scape/images/f/f1/Herblore-icon.png",
+	"Master herb cleaner",
+	"July 08/2014"]
+/*Denieru
+	[3605992,
+	"Highly Trusted","http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/highlytrusted01_zpsbb80c94e.png",
+	"Highly Trusted Coin Swapper",
+	"June 03/2014"],
+		
+	[3605992,
+	"Recruitment Leader",
+	"http://i1279.photobucket.com/albums/y527/Inhaps/SM%20badges/recruitleader_zpsc73bd81d.png",
+	"Head of Recruitment",
+	"June 03/2014"]
+*/
+/* Template
+	[USER ID,
+	"Award Name",
+	"Award Image",
+	"Award Description",
+	"Date award was given"]
+End of template*/
+]}
+//]]>
